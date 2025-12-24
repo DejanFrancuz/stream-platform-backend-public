e2015fcb4f96468d2386c0aed2a73287820905a4
unknown <dfrancuz7620rn@raf.rs>
2025-12-24 17:33:43 +0100

    cloudfront urls added

--- src/main/java/main/config/CorsConfig.java
+++ src/main/java/main/config/CorsConfig.java
@@ -31,11 +31,11 @@ public class CorsConfig implements WebMvcConfigurer {
         return source;
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
-                .allowedMethods(corsProperties.getAllowedMethods().toArray(new String[0]))
-                .allowedHeaders(corsProperties.getAllowedHeaders().toArray(new String[0]));
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
+//                .allowedMethods(corsProperties.getAllowedMethods().toArray(new String[0]))
+//                .allowedHeaders(corsProperties.getAllowedHeaders().toArray(new String[0]));
+//    }
 }
